2e88dc09fcbc106cca6fc21415007b91f38ac0fc
Zerch Dev <sergiog19zzh@gmail.com>
2025-01-15 21:40:58 -0600

    Se reemplazo el codigo de encapsulamiento por las anotaciones @Getter                     @Setter                     @AllArgsConstructor                     @NoArgsConstructor

--- ClinicaPrivada (1)/ClinicaPrivada/src/main/java/entidad/Medico.java
+++ ClinicaPrivada (1)/ClinicaPrivada/src/main/java/entidad/Medico.java
@@ -3,11 +3,17 @@ package entidad;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "medicos")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Medico {
 
     @Id
@@ -21,67 +27,4 @@ public class Medico {
     private String email;
     private String especialidad;
 
-    public int getId() {
-        return id;
-    }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellidoPaterno() {
-        return apellidoPaterno;
-    }
-
-    public void setApellidoPaterno(String apellidoPaterno) {
-        this.apellidoPaterno = apellidoPaterno;
-    }
-
-    public String getApellidoMaterno() {
-        return apellidoMaterno;
-    }
-
-    public void setApellidoMaterno(String apellidoMaterno) {
-        this.apellidoMaterno = apellidoMaterno;
-    }
-
-    public int getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(int telefono) {
-        this.telefono = telefono;
-    }
-
-    public String getCedula() {
-        return cedula;
-    }
-
-    public void setCedula(String cedula) {
-        this.cedula = cedula;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getEspecialidad() {
-        return especialidad;
-    }
-
-    public void setEspecialidad(String especialidad) {
-        this.especialidad = especialidad;
-    }
-}
